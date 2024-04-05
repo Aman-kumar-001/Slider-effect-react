32141f480e1802be46dc104678fa881a16d08d77
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-04-05 13:03:15 +0530

    done

--- sliding-banner/src/component/swipper.jsx
+++ sliding-banner/src/component/swipper.jsx
@@ -1,134 +1,143 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { A11y, Navigation, Pagination, Scrollbar } from 'swiper/modules';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { A11y, Navigation, Pagination, Scrollbar } from "swiper/modules";
 //
-import 'swiper/css';
+import "swiper/css";
 import "../component/swipper.css";
 
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import 'swiper/css/a11y';
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/a11y";
 
 function Swipper() {
   return (
     <Swiper
-    modules={[Navigation, Pagination , A11y, Scrollbar ]}
+      modules={[Navigation, Pagination, A11y, Scrollbar]}
       spaceBetween={50}
       slidesPerView={1}
-     
-       a11y={true}
+      a11y={true}
       navigation={true}
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
-      onSlideChange={() => console.log('slide change')}
+      onSlideChange={() => console.log("slide change")}
       onSwiper={(swiper) => console.log(swiper)}
     >
       <SwiperSlide>
-      <div className="first">
-            <div className="h1">
-              <h1>WE PROVIDE BEST</h1>
-              <h1>HEALTHCARE</h1>
-            </div>
-            <div className="para">
-              <p>
-                Explicabo esse amet tempora quibusdam laudantium, laborum eaque
-                magnam fugiat hic? Esse dicta aliquid error repudiandae earum
-                suscipit fugiat molestias, veniam, vel architecto veritatis
-                delectus repellat modi impedit sequi.
-              </p>
-            </div>
-            <div>
-              <button className="slide-button">Read More</button>
-            </div>
+        <div className="first">
+          <div className="h1">
+            <h1>WE PROVIDE BEST</h1>
+            <h1>HEALTHCARE</h1>
           </div>
+          <div className="para">
+            <p>
+              Explicabo esse amet tempora quibusdam laudantium, laborum eaque
+              magnam fugiat hic? Esse dicta aliquid error repudiandae earum
+              suscipit fugiat molestias, veniam, vel architecto veritatis
+              delectus repellat modi impedit sequi.
+            </p>
+          </div>
+          <div>
+            <button className="slide-button">Read More</button>
+          </div>
+        </div>
       </SwiperSlide>
-      <SwiperSlide><div className="first">
-            <div className="h1">
-              <h1>WE PROVIDE BEST</h1>
-              <h1>HEALTHCARE</h1>
-            </div>
-            <div className="para">
-              <p>
-                Explicabo esse amet tempora quibusdam laudantium, laborum eaque
-                magnam fugiat hic? Esse dicta aliquid error repudiandae earum
-                suscipit fugiat molestias, veniam, vel architecto veritatis
-                delectus repellat modi impedit sequi.
-              </p>
-            </div>
-            <div>
-              <button className="slide-button">Read More</button>
-            </div>
-          </div></SwiperSlide>
-      <SwiperSlide><div className="first">
-            <div className="h1">
-              <h1>WE PROVIDE BEST</h1>
-              <h1>HEALTHCARE</h1>
-            </div>
-            <div className="para">
-              <p>
-                Explicabo esse amet tempora quibusdam laudantium, laborum eaque
-                magnam fugiat hic? Esse dicta aliquid error repudiandae earum
-                suscipit fugiat molestias, veniam, vel architecto veritatis
-                delectus repellat modi impedit sequi.
-              </p>
-            </div>
-            <div>
-              <button className="slide-button">Read More</button>
-            </div>
-          </div></SwiperSlide>
-      <SwiperSlide><div className="first">
-            <div className="h1">
-              <h1>WE PROVIDE BEST</h1>
-              <h1>HEALTHCARE</h1>
-            </div>
-            <div className="para">
-              <p>
-                Explicabo esse amet tempora quibusdam laudantium, laborum eaque
-                magnam fugiat hic? Esse dicta aliquid error repudiandae earum
-                suscipit fugiat molestias, veniam, vel architecto veritatis
-                delectus repellat modi impedit sequi.
-              </p>
-            </div>
-            <div>
-              <button className="slide-button">Read More</button>
-            </div>
-          </div></SwiperSlide>
-      <SwiperSlide><div className="first">
-            <div className="h1">
-              <h1>WE PROVIDE BEST</h1>
-              <h1>HEALTHCARE</h1>
-            </div>
-            <div className="para">
-              <p>
-                Explicabo esse amet tempora quibusdam laudantium, laborum eaque
-                magnam fugiat hic? Esse dicta aliquid error repudiandae earum
-                suscipit fugiat molestias, veniam, vel architecto veritatis
-                delectus repellat modi impedit sequi.
-              </p>
-            </div>
-            <div>
-              <button className="slide-button">Read More</button>
-            </div>
-          </div></SwiperSlide>
-      <SwiperSlide><div className="first">
-            <div className="h1">
-              <h1>WE PROVIDE BEST</h1>
-              <h1>HEALTHCARE</h1>
-            </div>
-            <div className="para">
-              <p>
-                Explicabo esse amet tempora quibusdam laudantium, laborum eaque
-                magnam fugiat hic? Esse dicta aliquid error repudiandae earum
-                suscipit fugiat molestias, veniam, vel architecto veritatis
-                delectus repellat modi impedit sequi.
-              </p>
-            </div>
-            <div>
-              <button className="slide-button">Read More</button>
-            </div>
-          </div></SwiperSlide>
-     {/*  <SwiperSlide><div className="first">
+      <SwiperSlide>
+        <div className="first">
+          <div className="h1">
+            <h1>WE PROVIDE BEST</h1>
+            <h1>HEALTHCARE</h1>
+          </div>
+          <div className="para">
+            <p>
+              Explicabo esse amet tempora quibusdam laudantium, laborum eaque
+              magnam fugiat hic? Esse dicta aliquid error repudiandae earum
+              suscipit fugiat molestias, veniam, vel architecto veritatis
+              delectus repellat modi impedit sequi.
+            </p>
+          </div>
+          <div>
+            <button className="slide-button">Read More</button>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="first">
+          <div className="h1">
+            <h1>WE PROVIDE BEST</h1>
+            <h1>HEALTHCARE</h1>
+          </div>
+          <div className="para">
+            <p>
+              Explicabo esse amet tempora quibusdam laudantium, laborum eaque
+              magnam fugiat hic? Esse dicta aliquid error repudiandae earum
+              suscipit fugiat molestias, veniam, vel architecto veritatis
+              delectus repellat modi impedit sequi.
+            </p>
+          </div>
+          <div>
+            <button className="slide-button">Read More</button>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="first">
+          <div className="h1">
+            <h1>WE PROVIDE BEST</h1>
+            <h1>HEALTHCARE</h1>
+          </div>
+          <div className="para">
+            <p>
+              Explicabo esse amet tempora quibusdam laudantium, laborum eaque
+              magnam fugiat hic? Esse dicta aliquid error repudiandae earum
+              suscipit fugiat molestias, veniam, vel architecto veritatis
+              delectus repellat modi impedit sequi.
+            </p>
+          </div>
+          <div>
+            <button className="slide-button">Read More</button>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="first">
+          <div className="h1">
+            <h1>WE PROVIDE BEST</h1>
+            <h1>HEALTHCARE</h1>
+          </div>
+          <div className="para">
+            <p>
+              Explicabo esse amet tempora quibusdam laudantium, laborum eaque
+              magnam fugiat hic? Esse dicta aliquid error repudiandae earum
+              suscipit fugiat molestias, veniam, vel architecto veritatis
+              delectus repellat modi impedit sequi.
+            </p>
+          </div>
+          <div>
+            <button className="slide-button">Read More</button>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="first">
+          <div className="h1">
+            <h1>WE PROVIDE BEST</h1>
+            <h1>HEALTHCARE</h1>
+          </div>
+          <div className="para">
+            <p>
+              Explicabo esse amet tempora quibusdam laudantium, laborum eaque
+              magnam fugiat hic? Esse dicta aliquid error repudiandae earum
+              suscipit fugiat molestias, veniam, vel architecto veritatis
+              delectus repellat modi impedit sequi.
+            </p>
+          </div>
+          <div>
+            <button className="slide-button">Read More</button>
+          </div>
+        </div>
+      </SwiperSlide>
+      {/*  <SwiperSlide><div className="first">
             <div className="h1">
               <h1>WE PROVIDE BEST</h1>
               <h1>HEALTHCARE</h1>
@@ -145,9 +154,8 @@ function Swipper() {
               <button className="slide-button">Read More</button>
             </div>
           </div></SwiperSlide> */}
-     
     </Swiper>
-  )
+  );
 }
 
 export default Swipper;
